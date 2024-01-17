6df029c65d4726f9b2ca8a780ee994214902d790
Akbarshoh1701 <akbarsuvonov5157@gmail.com>
2024-01-17 18:02:30 +0500

    feat:I added the News model to the project

--- news/models.py
+++ news/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
 class User(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     eamil = models.EmailField(max_length=250)
     age = models.IntegerField()
+
+
+class News(models.Model):
+    tite = models.CharField(max_length=250)
+    img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    data = models.DateField()
+    text = models.TextField()
+
