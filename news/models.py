e2ec6cb5205322c30a5cb5e3c5ce569511b408f5
Akbarshoh1701 <akbarsuvonov5157@gmail.com>
2024-01-17 16:47:04 +0500

    feat:I added the User model to the project

--- news/models.py
+++ news/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    eamil = models.EmailField(max_length=250)
+    age = models.IntegerField()
